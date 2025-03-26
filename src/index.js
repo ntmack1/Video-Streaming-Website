c20f7b127aad66f79d0fa69eb7d031aa7837d524
ntmack1 <ntmack5@gmail.com>
2025-03-26 01:09:12 -0500

    fix: Correct typos preventing video loading error

--- src/index.js
+++ src/index.js
@@ -6,16 +6,17 @@ const app = express();
 const port = 3000;
 
 app.get("/video", async (req, res) => {
-    const videoPath = "../videos/SampleVideo_1280x720_lmb.mp4";
+    const videoPath = "../videos/SampleVideo_1280x720_1mb.mp4";
     const stats = await fs.promises.stat(videoPath);
 
     res.writeHead(200, {
         "Content-Length": stats.size,
         "Content-Type": "video/mp4",
     });
-    fs.createReadStream(videoPath),pip(res);
+    fs.createReadStream(videoPath).pipe(res);
 });
 
 app.listen(port);
 
 console.log(`Listening on port ${port}`);
+
