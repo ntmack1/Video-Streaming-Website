4a4623d3bf92760e97a67de295e86b96f267fff8
ntmack1 <ntmack5@gmail.com>
2025-03-26 00:22:47 -0500

    feat: Configure Express server to serve video files

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 const express = require('express');
+const fs = require("fs");
+
 const app = express();
+
 const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-}); 
+app.get("/video", async (req, res) => {
+    const videoPath = "../videos/SampleVideo_1280x720_lmb.mp4";
+    const stats = await fs.promises.stat(videoPath);
+
+    res.writeHead(200, {
+        "Content-Length": stats.size,
+        "Content-Type": "video/mp4",
+    });
+    fs.createReadStream(videoPath),pip(res);
+});
 
 app.listen(port);
 
