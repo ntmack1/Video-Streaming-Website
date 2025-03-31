d4a6085847a5eede688218cbe0330b685b6fb7c3
ntmack1 <ntmack5@gmail.com>
2025-03-31 01:31:42 -0500

    fix: Fix application-breaking bug

--- video-streaming/src/index.js
+++ video-streaming/src/index.js
@@ -1,13 +1,22 @@
-const express = require('express');
+const express = require("express");
 const http = require("http");
 
 if (!process.env.PORT) {
     throw new Error("Please specify the port number for the HTTP server with the environment variable PORT.");
 }
 
+if (!process.env.VIDEO_STORAGE_HOST) {
+    throw new Error("Please specify the host name for the video storage microservice in variable VIDEO_STORAGE_HOST.");
+}
+
+if (!process.env.VIDEO_STORAGE_PORT) {
+    throw new Error("Please specify the port number for the video storage microservice in variable VIDEO_STORAGE_PORT.");
+}
+
 const PORT = process.env.PORT;
 const VIDEO_STORAGE_HOST = process.env.VIDEO_STORAGE_HOST;
 const VIDEO_STORAGE_PORT = parseInt(process.env.VIDEO_STORAGE_PORT);
+console.log(`Forwarding video requests to ${VIDEO_STORAGE_HOST}:${VIDEO_STORAGE_PORT}.`);
 
 const app = express();
 
@@ -16,12 +25,12 @@ app.get("/video", (req, res) => {
         {
             host: VIDEO_STORAGE_HOST,
             port: VIDEO_STORAGE_PORT,
-            path:'video?path=SampleVideo_1280x720_1mb.mp4',
+            path:'/video?path=SampleVideo_1280x720_1mb.mp4',
             method: 'GET',
             headers: req.headers
         },
         forwardResponse => {
-            res.writeHead(forwardResponse.statusCode,forwardResponse.headers);
+            res.writeHeader(forwardResponse.statusCode,forwardResponse.headers);
             forwardResponse.pipe(res);
         }
     );
