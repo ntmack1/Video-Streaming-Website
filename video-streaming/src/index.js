354f2adbe0ad33daf2e67a6372775e5ea742c74c
ntmack1 <ntmack5@gmail.com>
2025-04-06 17:23:24 -0500

    fix: Install mongodb, fix spelling errors

--- video-streaming/src/index.js
+++ video-streaming/src/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const http = require("http");
+const mongodb = require("mongodb");
 
 if (!process.env.PORT) {
     throw new Error("Please specify the port number for the HTTP server with the environment variable PORT.");
@@ -13,31 +14,62 @@ if (!process.env.VIDEO_STORAGE_PORT) {
     throw new Error("Please specify the port number for the video storage microservice in variable VIDEO_STORAGE_PORT.");
 }
 
+if (!process.env.DBHOST) {
+    throw new Error("Please specify the host databse for the video storage microservice in variable DBHOST.");
+}
+
+if (!process.env.DBNAME) {
+    throw new Error("Please specify the database name for the video storage microservice in variable DBNAME.");
+}
+
 const PORT = process.env.PORT;
 const VIDEO_STORAGE_HOST = process.env.VIDEO_STORAGE_HOST;
 const VIDEO_STORAGE_PORT = parseInt(process.env.VIDEO_STORAGE_PORT);
+const DBHOST = process.env.DBHOST;
+const DBNAME = process.env.DBNAME;
+
 console.log(`Forwarding video requests to ${VIDEO_STORAGE_HOST}:${VIDEO_STORAGE_PORT}.`);
 
-const app = express();
+async function main () {
+    const client = await mongodb.MongoClient.connect (DBHOST);
+    const db = client.db(DBNAME);
+    const videosCollection = db.collection("videos");
 
-app.get("/video", (req, res) => {
-    const forwardRequest = http.request (
-        {
-            host: VIDEO_STORAGE_HOST,
-            port: VIDEO_STORAGE_PORT,
-            path:'/video?path=SampleVideo_1280x720_1mb.mp4',
-            method: 'GET',
-            headers: req.headers
-        },
-        forwardResponse => {
-            res.writeHeader(forwardResponse.statusCode,forwardResponse.headers);
-            forwardResponse.pipe(res);
+
+    const app = express();
+
+    app.get("/video", async (req, res) => {
+        const videoId = new mongodb.ObjectId(req.query.id);
+        const videoRecord = await videosCollection.findOne({_id: videoId });
+        
+        if (!videoRecord) {
+            res.sendStatus(404);
+            return;
         }
-    );
 
-    req.pipe(forwardRequest);
-});
+        const forwardRequest = http.request (
+            {
+                host: VIDEO_STORAGE_HOST,
+                port: VIDEO_STORAGE_PORT,
+                path:`/video?path=${videoRecord.videoPath}`,
+                method: 'GET',
+                headers: req.headers
+            },
+            forwardResponse => {
+                res.writeHeader(forwardResponse.statusCode,forwardResponse.headers);
+                forwardResponse.pipe(res);
+            }
+        );
 
-app.listen(PORT, () => {
-    console.log(`Microservice online`);
+        req.pipe(forwardRequest);
+    });
+
+    app.listen(PORT, () => {
+        console.log(`Microservice online`);
+    });
+}
+
+main().catch(err => {
+    console.error("Microservices failed to start.");
+    console.error(err && err.stack || err);
 });
